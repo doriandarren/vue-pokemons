8920c584de363442a26d03d493636f3bcbce315f
Dorian Gonzalez <doriandarren1@gmail.com>
2022-11-03 22:27:10 +0100

    First commit

---
+++ src/views/PokemonsView.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+import axios from 'axios';
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+
+
+const pokemons = ref([]);
+
+const getData = async () => {
+    try {
+        const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon/');
+        pokemons.value = data.results;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getData();
+
+</script>
+
+<template>
+
+    <h1>Pokemones</h1>
+
+    <ul>
+        <li 
+            v-for="pokemon in pokemons"
+            :key="pokemon.name"
+        >
+        <router-link :to="`/pokemons/${pokemon.name}`">{{ pokemon.name }}</router-link>
+        
+        </li>
+    </ul>
+
+</template>
+
+<style>
+
+</style>
